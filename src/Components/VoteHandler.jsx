a668d085eddd0de94343ca15b4efe51e9d30c2c8
Kathryn Roberts <kathrynr22@Kathryns-MacBook-Air.local>
2020-05-27 15:03:30 +0100

    event console loggin

--- src/Components/VoteHandler.jsx
+++ src/Components/VoteHandler.jsx
@@ -8,6 +8,8 @@ class VoteHandler extends Component {
 
   handleUpVote = (event) => {
     this.setState(({ inc_votes }) => {
+      console.log("set state");
+      console.log(event); // 1
       return {
         inc_votes: inc_votes + 1,
       };
@@ -15,6 +17,7 @@ class VoteHandler extends Component {
 
     const { article_id, comment_id } = this.props;
     const { inc_votes } = this.state;
+    console.log("hello from below setState");
     if (article_id) {
       return api.patchArticleVote(article_id, inc_votes);
     } else if (comment_id) {
@@ -39,16 +42,22 @@ class VoteHandler extends Component {
 
   render() {
     const { inc_votes } = this.state;
-    const { votes } = this.props;
+    const { votes, article_id, comment_id } = this.props;
     return (
       <>
         <p>Votes: {votes + inc_votes} </p>
-        <button onClick={this.handleUpVote} disabled={inc_votes > 0}>
+        <button
+          onClick={() => this.handleUpVote(1, article_id, comment_id)}
+          disabled={inc_votes > 0}
+        >
           <span role="img" aria-label="thumb-up">
             👍
           </span>
         </button>
-        <button onClick={this.handleDownVote} disabled={inc_votes < 0}>
+        <button
+          onClick={() => this.handleDownVote(-1, article_id, comment_id)}
+          disabled={inc_votes < 0}
+        >
           <span role="img" aria-label="thumb-down">
             👎
           </span>
