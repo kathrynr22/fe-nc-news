73222eee36144fcc7c3847b3e13ed7fb43be155e
Kathryn Roberts <kathrynr22@Kathryns-MacBook-Air.local>
2020-05-29 09:23:13 +0100

    refactored api to use baseurl

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,16 +1,14 @@
 import axios from "axios";
-
+const baseURL = "https://kathryn-nc-news.herokuapp.com/api";
 export const getTopics = () => {
-  return axios
-    .get("https://kathryn-nc-news.herokuapp.com/api/topics")
-    .then(({ data: { topics } }) => {
-      return topics;
-    });
+  return axios.get(`${baseURL}/topics`).then(({ data: { topics } }) => {
+    return topics;
+  });
 };
 
 export const getArticleById = (article_id) => {
   return axios
-    .get(`https://kathryn-nc-news.herokuapp.com/api/articles/${article_id}`)
+    .get(`${baseURL}/articles/${article_id}`)
     .then(({ data: { articleById } }) => {
       return articleById;
     });
@@ -18,20 +16,15 @@ export const getArticleById = (article_id) => {
 
 export const getComments = (article_id) => {
   return axios
-    .get(
-      `https://kathryn-nc-news.herokuapp.com/api/articles/${article_id}/comments`
-    )
+    .get(`${baseURL}/articles/${article_id}/comments`)
     .then(({ data: { commentsByArticleId } }) => {
       return commentsByArticleId;
     });
 };
 
 export const getArticles = (topic, sort_by, order) => {
-  console.log("inside axios");
-  console.log(sort_by);
-
   return axios
-    .get("https://kathryn-nc-news.herokuapp.com/api/articles", {
+    .get(`${baseURL}/articles`, {
       params: {
         topic: topic,
         sort_by: sort_by,
@@ -43,22 +36,18 @@ export const getArticles = (topic, sort_by, order) => {
 };
 
 export const patchArticleVote = (article_id, inc_votes) => {
-  console.log("inside axios patch article");
-  console.log(inc_votes);
   return axios
-    .patch(`https://kathryn-nc-news.herokuapp.com/api/articles/${article_id}`, {
+    .patch(`${baseURL}/articles/${article_id}`, {
       inc_votes: inc_votes,
     })
     .then(({ data: { patchedArticle } }) => {
-      console.log("inside then");
-      console.log(patchedArticle);
       return patchedArticle;
     });
 };
 
 export const patchCommentVote = (comment_id, inc_votes) => {
   return axios
-    .patch(`https://kathryn-nc-news.herokuapp.com/api/comments/${comment_id}`, {
+    .patch(`${baseURL}/comments/${comment_id}`, {
       inc_votes: inc_votes,
     })
     .then(({ data: { patchedComment } }) => {
@@ -68,20 +57,15 @@ export const patchCommentVote = (comment_id, inc_votes) => {
 
 export const postComment = (article_id, username, body) => {
   return axios
-    .post(
-      `https://kathryn-nc-news.herokuapp.com/api/articles/${article_id}/comments`,
-      {
-        username,
-        body,
-      }
-    )
+    .post(`${baseURL}/articles/${article_id}/comments`, {
+      username,
+      body,
+    })
     .then(({ data: { postedComment } }) => {
       return postedComment;
     });
 };
 
 export const deleteCommentByCommentId = (comment_id) => {
-  return axios.delete(
-    `https://kathryn-nc-news.herokuapp.com/api/comments/${comment_id}`
-  );
+  return axios.delete(`${baseURL}/comments/${comment_id}`);
 };
