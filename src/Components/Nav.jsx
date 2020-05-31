8b824c8f07dc2ee12b3d69df38f3e68a5956bee8
Kathryn Roberts <kathrynr22@Kathryns-MacBook-Air.local>
2020-05-31 16:01:40 +0100

    styling

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -8,14 +8,18 @@ class Nav extends Component {
   };
   render() {
     return (
-      <nav>
-        <Link to="/">
+      <nav className="nav">
+        <Link to="/" className="home-link">
           <p>Home</p>
         </Link>
 
         {this.state.topics.map((topic) => {
           return (
-            <Link to={`articles/topic/${topic.slug}`} key={topic.slug}>
+            <Link
+              to={`articles/topic/${topic.slug}`}
+              key={topic.slug}
+              className="topic-name"
+            >
               <p>{topic.slug.charAt(0).toUpperCase() + topic.slug.slice(1)}</p>
             </Link>
           );
